e98fde94971e753fc86f7e88476cc5ce5b989549
Sachin120123 <120398205+Sachin120123@users.noreply.github.com>
2025-11-21 17:48:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello DevOps Cloud World. !!!!from Netra ');
+  res.send('Hello DevOps Cloud World. !!!!from Netra Chauhan ');
 });
 
 app.listen(PORT, HOST);
